f3fcc7630488e03ae9521a7b2274dacc3384e055
ysy <1838940294@qq.com>
2022-04-06 19:46:03 +0800

    fix: delete previous webglengine and program

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,3 @@
 export { Matrix4 } from './math/Matrix4';
 export { Vector4 } from './math/Vector4';
-export { Program } from './core/Program';
-export { WebGLEngine } from './core/WebGLEngine';
+export { WebGLEngine } from './webgl/WebGLEngine';
