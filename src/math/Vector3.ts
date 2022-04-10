5451a235799f8e0270889d6cc0c4bbf0a2791771
ysy <1838940294@qq.com>
2022-04-10 10:23:07 +0800

    feat: vector2 and vector3 in math

---
+++ src/math/Vector3.ts
@@ -0,0 +1,11 @@
+export class Vector3 {
+  x: number;
+  y: number;
+  z: number;
+
+  constructor(x: number = 0, y: number = 0, z: number = 0) {
+    this.x = x;
+    this.y = y;
+    this.z = z;
+  }
+}
