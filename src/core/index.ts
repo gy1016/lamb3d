ee4da67803cfb03ada96144738f998a90d5c4c4d
ysy <1838940294@qq.com>
2022-04-22 17:06:58 +0800

    chore: add export module in src/index.ts

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,1 +1,6 @@
-export { WebCanvas } from './WebCanvas';
+export { Canvas } from './Canvas';
+export { Program } from './Program';
+export { Render } from './Render';
+export { RenderInfo } from './RenderInfo';
+export { Scene } from './Scene';
+export { Engine } from './Engine';
