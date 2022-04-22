7f438d9d8a5e97f72c92dc26ac100c732f1ddc61
ysy <1838940294@qq.com>
2022-04-22 14:36:20 +0800

    feat: add WebCanvas into lamb3d

---
+++ src/core/WebCanvas.ts
@@ -0,0 +1,47 @@
+export class WebCanvas {
+  gl: WebGLRenderingContext;
+  _webCanvas: HTMLCanvasElement;
+
+  private _width: number;
+  private _height: number;
+
+  get width(): number {
+    return this._width;
+  }
+
+  set width(value: number) {
+    if (this._width !== value) {
+      this._width = value;
+    }
+  }
+
+  get height(): number {
+    return this._height;
+  }
+
+  set height(value: number) {
+    if (this._height !== value) {
+      this._height = value;
+    }
+  }
+
+  constructor(canvasId: string) {
+    const canvas = document.getElementById(canvasId);
+    if (!(canvas instanceof HTMLCanvasElement)) {
+      throw new Error(`${canvasId} is not a HTMLCanvasElement`);
+    }
+    this._webCanvas = canvas;
+    this.gl = canvas.getContext('webgl');
+    if (this.gl === null) {
+      throw new Error(`Unable to initialize the webgl context`);
+    }
+  }
+
+  resizeByClientSize(pixelRatio: number = window.devicePixelRatio): void {
+    const webCanvas = this._webCanvas;
+    if (webCanvas instanceof HTMLCanvasElement) {
+      this.width = webCanvas.clientWidth * pixelRatio;
+      this.height = webCanvas.clientHeight * pixelRatio;
+    }
+  }
+}
--- src/math/index.ts
+++ src/math/index.ts
@@ -1,2 +1,3 @@
 export { Vector3 } from './Vector3';
 export { Vector4 } from './Vector4';
+export { MathUtil } from './MathUtil';
