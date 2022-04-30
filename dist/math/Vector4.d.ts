7990b0ca7f106cf2a8c8a367ad9146e8b71a6a00
ysy <1838940294@qq.com>
2022-04-30 15:54:17 +0800

    test: modify index.html to test new function

--- dist/math/Vector4.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class Vector4 {
-    x: number;
-    y: number;
-    z: number;
-    w: number;
-    constructor(x: number, y: number, z: number, w: number);
-    copyFrom(vec4: Vector4): void;
-}
