644d3d2b435ebc9d94df446f361af3e1d55f1fce
ysy <1838940294@qq.com>
2022-05-08 19:18:30 +0800

    fix: initial xyz/xyzw useing zero in constructor

--- src/math/Vector3.ts
+++ src/math/Vector3.ts
@@ -3,7 +3,7 @@ export class Vector3 {
   y: number;
   z: number;
 
-  constructor(x: number, y: number, z: number) {
+  constructor(x: number = 0, y: number = 0, z: number = 0) {
     this.x = x;
     this.y = y;
     this.z = z;
--- src/math/Vector4.ts
+++ src/math/Vector4.ts
@@ -4,7 +4,7 @@ export class Vector4 {
   z: number;
   w: number;
 
-  constructor(x: number, y: number, z: number, w: number) {
+  constructor(x: number = 0, y: number = 0, z: number = 0, w: number = 0) {
     this.x = x;
     this.y = y;
     this.z = z;
