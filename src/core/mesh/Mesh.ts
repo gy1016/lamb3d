33e666783b1c44e274209c143dc871db614b7d01
ysy <1838940294@qq.com>
2022-05-12 11:30:37 +0800

    chore: simplify Mesh.ts

--- src/core/mesh/Mesh.ts
+++ src/core/mesh/Mesh.ts
@@ -1,21 +1,5 @@
-import { BufferUtil } from '../graphic/BufferUtil';
-import { MeshTopology } from '../graphic/enums/MeshTopology';
-import { IndexBufferBinding } from '../graphic/IndexBufferBinding';
-import { VertexBufferBinding } from '../graphic/VertexBufferBinding';
-import { VertexElement } from '../graphic/VertexElement';
-import { ShaderProgram } from '../shader/ShaderProgram';
-
 export class Mesh {
   name: string;
-
-  _vertexElementMap: Record<string, VertexElement> = {};
-  _glIndexType: number;
-  _glIndexByteCount: number;
-
-  _instanceCount: number = 0;
-  _vertexBufferBindings: VertexBufferBinding[] = [];
-  _indexBufferBinding: IndexBufferBinding = null;
-  _vertexElements: VertexElement[] = [];
 
   private _positions: number[] = [];
   private _normals: number[] = [];
@@ -56,26 +40,5 @@ export class Mesh {
 
   getIndices(): Uint8Array | Uint16Array {
     return this._indices;
-  }
-
-  _clearVertexElements(): void {
-    this._vertexElements.length = 0;
-    const vertexElementMap = this._vertexElementMap;
-    for (const k in vertexElementMap) {
-      delete vertexElementMap[k];
-    }
-  }
-
-  _addVertexElement(element: VertexElement): void {
-    const { semantic } = element;
-    this._vertexElementMap[semantic] = element;
-    this._vertexElements.push(element);
-  }
-
-  protected _setVertexElements(elements: VertexElement[]): void {
-    this._clearVertexElements();
-    for (let i = 0, n = elements.length; i < n; i++) {
-      this._addVertexElement(elements[i]);
-    }
   }
 }
