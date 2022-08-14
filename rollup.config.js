eb864861d46c876999066b16f8e3d492cdadcf7d
lamb <1838940294@qq.com>
2022-08-14 16:09:03 +0800

    feat: add dev mode

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,12 @@
-// import babel from '@rollup/plugin-babel';
 import resolve from '@rollup/plugin-node-resolve';
-import typescript from '@rollup/plugin-typescript';
+import typescript from 'rollup-plugin-typescript2';
 import commonjs from '@rollup/plugin-commonjs';
 import glslify from 'rollup-plugin-glslify';
+import serve from 'rollup-plugin-serve';
+
+const path = require('path');
+
+const { NODE_ENV } = process.env;
 
 export default [
   {
@@ -25,7 +29,14 @@ export default [
         include: [/\.glsl$/],
       }),
       commonjs(),
-      typescript({ sourceMap: true }),
+      typescript({ tsconfig: path.resolve(__dirname, 'tsconfig.json') }),
+      NODE_ENV === 'development'
+        ? serve({
+            open: true,
+            contentBase: ['public', 'dist'],
+            port: 9999,
+          })
+        : null,
     ],
   },
 ];
