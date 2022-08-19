a9cc5cae7343a7acf11b494a500d108f53611a27
lamb <1838940294@qq.com>
2022-08-19 17:17:30 +0800

    feat: add config floder

---
+++ src/config/skybox.ts
@@ -0,0 +1,8 @@
+export const defaultSky = {
+  positiveX: 'http://121.199.160.202/images/skybox/tycho2t3_80_mx.jpg',
+  negativeX: 'http://121.199.160.202/images/skybox/tycho2t3_80_px.jpg',
+  positiveY: 'http://121.199.160.202/images/skybox/tycho2t3_80_py.jpg',
+  negativeY: 'http://121.199.160.202/images/skybox/tycho2t3_80_my.jpg',
+  positiveZ: 'http://121.199.160.202/images/skybox/tycho2t3_80_mz.jpg',
+  negativeZ: 'http://121.199.160.202/images/skybox/tycho2t3_80_pz.jpg',
+};
